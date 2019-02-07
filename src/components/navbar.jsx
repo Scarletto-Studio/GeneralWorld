01cb5aa2762a3878d0232e3a2c4174add44161d0
lebrancconvas <lebranccconvas@gmail.com>
2019-02-08 02:59:19 +0700

    Add Router.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Styled from "styled-components";
+import {Route} from 'react-router-dom'
+import World from '../pages/world.jsx'
+import Content from '../components/content.jsx'
 // import './style/navbar.css'
 
 const Nav = Styled.ul`
@@ -25,11 +28,17 @@ class Navbar extends Component {
     return (
       <div>
         <Nav>
-          <List><a href="#">Home</a></List>
-          <List><a href="#">World</a></List>
-          <List><a href="#">Content</a></List>
-          <List><a href="#">About Us</a></List>
+          <List><a href="/">Home</a></List>
+          <List><a href="/world">World</a></List>
+          <List><a href="/content">Content</a></List>
+          <List><a href="/about">About Us</a></List>
         </Nav>
+        <div>
+          <Route path="/" />
+          <Route path="/world" component={World} />
+          <Route path="/content" component={Content} />
+          <Route path="/about" />
+        </div>
       </div>
     );
   }
---
+++ src/pages/world.jsx
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+import '../App.css'
+
+class World extends Component {
+    render() {
+        return(
+            <div className="App-header">
+                <h1>Hello Pages.</h1>
+            </div>
+        )
+    }
+}
+
+export default World
