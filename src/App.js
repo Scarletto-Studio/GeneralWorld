b23853eda2362d00c8879ed4a2bed77d1f94b977
lebrancconvas <lebranccconvas@gmail.com>
2019-02-07 20:40:54 +0700

    Add Navbar Link.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
 import './App.css';
 
 class App extends Component {
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import Styled from "styled-components";
+// import './style/navbar.css'
 
 const Nav = Styled.ul`
     list-style-type: none;
     margin: 0;
     padding: 0;
+    overflow: hidden;
+    background-color: black;
 `;
+
+const List = Styled.li`
+  display: inline;
+  margin: 10px;
+  &:hover {
+    background-color: white;
+  }
+`
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>Home</li>
-          <li>World</li>
-          <li>Content</li>
-          <li>About Us</li>
-        </ul>
+        <Nav>
+          <List><a href="#">Home</a></List>
+          <List><a href="#">World</a></List>
+          <List><a href="#">Content</a></List>
+          <List><a href="#">About Us</a></List>
+        </Nav>
       </div>
     );
   }
