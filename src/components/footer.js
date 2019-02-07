1ad1514ffd999c0cb08de2aabd12fe0950a906a7
lebrancconvas <lebranccconvas@gmail.com>
2019-02-07 12:47:02 +0700

    Add Styled-Components and Edit Copyright Footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,18 @@
 import React , {Component} from 'react'
+import styled from 'styled-components'
+
+const Foot = styled.footer`
+    background: black;
+    color: white;
+    height: 100%;
+`
 
 class Footer extends Component {
     render() {
         return(
-            <footer>
-                <p>Create by Lebranc Convas</p>
-            </footer>
+            <Foot>
+                <p> Copyright© 2019 Scarletto Studio, All Rights Reserved </p>
+            </Foot>
         )
     }
 }
